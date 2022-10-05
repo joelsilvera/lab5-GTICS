eb5a9970c67ac9a1b7d5fefb8b5953c1a69630c8
elias <m.yovera@pucp.edu.pe>
2022-10-04 21:06:43 -0500

    Employee

--- src/main/java/com/pucp/lab5gtics/controller/EmployeeController.java
+++ src/main/java/com/pucp/lab5gtics/controller/EmployeeController.java
@@ -1,5 +1,9 @@
 package com.pucp.lab5gtics.controller;
 
+import com.pucp.lab5gtics.entity.Employee;
+import com.pucp.lab5gtics.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,8 +11,24 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.util.List;
+import java.util.Optional;
+
 @Controller
 public class EmployeeController {
+    @Autowired
+    EmployeeRepository employeeRepository;
+
+    @GetMapping({"empleado/info"})
+    public String infoempleado(Model model, @PathVariable(name = "id") Integer id){
+        Optional<Employee> optEmp = employeeRepository.findById(id);
+
+        //List<Employee> listaempleados = employeeRepository.findAll(Sort.by("first_name"));
+
+        //model.addAttribute("listaemployee", listaempleados);
+
+        return "employee/information";
+    }
 
 
     @GetMapping({"empleado/lista", "empleado"})
--- src/main/java/com/pucp/lab5gtics/repository/EmployeeRepository.java
+++ src/main/java/com/pucp/lab5gtics/repository/EmployeeRepository.java
@@ -1,8 +1,11 @@
 package com.pucp.lab5gtics.repository;
 
+import com.pucp.lab5gtics.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 //Completar
 @Repository
-public interface EmployeeRepository {
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
 }
