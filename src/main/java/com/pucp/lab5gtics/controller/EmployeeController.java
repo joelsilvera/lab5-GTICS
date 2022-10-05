00f9f68dd618de343c6607f9705d98319f155c6c
joelsilvera <joel.silvera@pucp.edu.pe>
2022-10-04 21:06:46 -0500

    commit

--- src/main/java/com/pucp/lab5gtics/controller/EmployeeController.java
+++ src/main/java/com/pucp/lab5gtics/controller/EmployeeController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -34,15 +35,15 @@ public class EmployeeController {
     }
 
     //Guardar Empleado
-    //@...Mapping("")
+    @PostMapping("empleado/guardar")
     public String saveEmployee(  ) {
         //        COMPLETAR
         return "XXXXXX";
     }
 
-    //Nuevo Empleado
+    @GetMapping("empleado/nuevo")
     public String newEmployee(Model model) {
         //        COMPLETAR
-        return "XXXXXX";
+        return "employee/newForm";
     }
 }
