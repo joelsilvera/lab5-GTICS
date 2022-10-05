10b7b90469650315018a3fedfaa03962bdb0a4c9
elias <m.yovera@pucp.edu.pe>
2022-10-04 21:43:23 -0500

    Employee

--- src/main/java/com/pucp/lab5gtics/repository/JobRepository.java
+++ src/main/java/com/pucp/lab5gtics/repository/JobRepository.java
@@ -1,9 +1,11 @@
 package com.pucp.lab5gtics.repository;
 
+import com.pucp.lab5gtics.entity.Employee;
 import com.pucp.lab5gtics.entity.Job;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 //Completar
-public interface JobRepository extends JpaRepository<Job, Integer> {
+@Repository
+public interface JobRepository extends JpaRepository<Job, String> {
 }
