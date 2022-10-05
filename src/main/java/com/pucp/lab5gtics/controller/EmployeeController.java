117ea7f4fc9b818b6e1582797a34c9017178bfa4
joelsilvera <joel.silvera@pucp.edu.pe>
2022-10-04 21:47:00 -0500

    linkeos

--- src/main/java/com/pucp/lab5gtics/controller/EmployeeController.java
+++ src/main/java/com/pucp/lab5gtics/controller/EmployeeController.java
@@ -3,12 +3,10 @@ package com.pucp.lab5gtics.controller;
 import com.pucp.lab5gtics.entity.Employee;
 import com.pucp.lab5gtics.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.persistence.Access;
@@ -27,6 +25,12 @@ public class EmployeeController {
     }
 
 
+    @GetMapping({"empleado/lista", "empleado"})
+    public String listEmployee(Model model, @RequestParam(name = "search",required = false) String search, @RequestParam(name = "order", required = false) Integer order, RedirectAttributes attributes){
+
+
+        return "XXXXXX";
+    }
 
 
     //Buscar Empleado
@@ -44,16 +48,15 @@ public class EmployeeController {
     }
 
     //Guardar Empleado
-    //@...Mapping("")
-    public String saveEmployee(  ) {
-        //        COMPLETAR
-        return "XXXXXX";
+    @PostMapping("/guardar")
+    public String saveEmployee(Employee employee) {
+        employeeRepository.save(employee);
+        return "redirect:/empleado/lista";
     }
 
-    //Nuevo Empleado
-    public String newEmployee(Model model) {
-        //        COMPLETAR
-        return "XXXXXX";
+    @GetMapping("/nuevo")
+    public String newEmployee() {
+        return "employee/newForm";
     }
 
     @GetMapping("/borrar")
